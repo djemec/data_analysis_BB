04286f1f4d098a12a28407ec49ada1bb1dbeeb23
Domen <domen.jemec@gmail.com>
2019-03-12 21:23:16 -0700

    Updates

---
+++  data_vis.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  2 13:19:49 2019
+
+@author: domenjemec
+"""
+
+import pandas as pd
+
+bb_raw
+
+def main():
+    global bb_raw
+    bb_raw = pd.read_csv('~/', parse_dates=true)
+    print(bb_raw.head())
+
+if __name__ == "__main__":
+    main()
